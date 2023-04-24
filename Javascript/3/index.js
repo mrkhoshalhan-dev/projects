4c42d0979617dd522032501a47e4b56fa583983e
Joseph Sony <126966283+joskeph@users.noreply.github.com>
2023-04-24 18:59:44 +0530

    changes

---
+++ Javascript/3/index.js
@@ -0,0 +1,11 @@
+let arr = [];
+arr.push("Joseph");
+arr.push("joskeph");
+function addE(ELEMENT){
+    arr.push(ELEMENT)
+}
+addE("the name");
+addE("addy")
+
+console.log("Length of the array:", arr.length)
+console.log("arr:", arr)
