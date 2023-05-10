e39801ee583927a58609072c53a53dfcc5cad1d8
Joseph Sony <126966283+joskeph@users.noreply.github.com>
2023-05-10 21:55:47 +0530

    changes

--- Javascript/3/index.js
+++ Javascript/3/index.js
@@ -3,7 +3,9 @@ let counter=0;
 document.querySelector("#increment").addEventListener("click",function(){
     counter++;
     document.getElementById("counter").innerText= counter;
-    document.querySelector("ul").appendChild(document.createElement('li'));
+    const li = document.createElement("li");
+    li.innerText= counter;
+    document.querySelector("ul").appendChild(li);
 })
 document.getElementById("decrement").addEventListener("click",function(){
     if(counter>0){
