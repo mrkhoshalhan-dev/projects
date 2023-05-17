617f6847b0bb23e8d7623aca5f8cd502757ae2ac
Joseph Sony <126966283+joskeph@users.noreply.github.com>
2023-05-17 08:53:29 +0530

    learing promises

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
-function fn(a,b){
-    if(a>b){
-        return a
-    }
+const getData = () =>{
+    return fetch("data.json")
 }
 
-const res = fn(100,10)
-
-res.then((result)=>{
-    console.log(result)
+getData().then(res=>{
+    console.log("pass 1")
+}).then(res2=>{
+    console.log("pass 2")
+}).then(res3=>{
+    console.log('pass 3')
+    throw new Error("this is just an error message")
+}).then(res4=>{
+    console.log("this should not be seen")
+}).catch(err=>{
+    console.log(err)
+}).then(res5=>{
+    console.log("pass 5")
 })
