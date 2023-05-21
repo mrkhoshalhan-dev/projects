7b67c3b6f5bb76b61cc94a8f2832a63fdfb30bdd
joskeph <joskeph@gmail.com>
2023-05-21 11:50:57 +0530

    changes

--- js/2/index.js
+++ js/2/index.js
@@ -1,6 +1,6 @@
 async function getData(){
     try{
-        const dat = await fetch("https://codedamn.com");
+        const dat = await fetch("https://google.com");
          console.log(dat)
     }catch(error){
         console.log("Sum Error",error)
